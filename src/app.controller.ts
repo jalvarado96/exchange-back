c457e378d32dcb2ed264ce0dd34b258331f6ab78
deiberman <deiberman@globaltask.net>
2021-12-01 12:32:46 -0300

    Configure JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,21 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private authService: AuthService) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user
-    }
+    return this.authService.login(req.user);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}

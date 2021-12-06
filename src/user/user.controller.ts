9ee43e8aa387f03302edc3ec54ebeba9e4b41380
DamiEiber <damieiber@gmail.com>
2021-12-06 11:40:51 -0300

    Swaggering User

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,45 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { UserUpdateDto } from './dto/updateUserDto';
+import { UserDto } from './dto/userDto';
+import { UserService } from './user.service';
+import { Request } from 'express'
 
 @ApiTags('Users')
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+    @Get()
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    findAll(
+        @Req() req: Request
+    ) {
+        console.log({
+            req
+        })
+        return this.userService.findAll();
+    }
+
+    @Post()
+    create(@Body() createUserDto: UserDto) {
+        return this.userService.create(createUserDto);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() updateUserDto: UserUpdateDto) {
+        return this.userService.update(id, updateUserDto);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(id);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        return this.userService.remove(id);
+    }
+}
+
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,48 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entity/User';
+import { UserUpdateDto } from './dto/updateUserDto';
+import { UserDto } from './dto/userDto';
 
 @Injectable()
 export class UserService {
-    private readonly users = [];
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
     ) { }
+
+    async findAll() {
+        return this.userRepository.find();
+    }
+
+    async create(createUserDto: UserDto) {
+        const userExists = await this.userRepository
+            .createQueryBuilder('user')
+            .where('user.username like :username', { name: `%${createUserDto.username}%` })
+            .getOne();
+        if (userExists) {
+            throw new ConflictException('Ya existe un user con ese nombre');
+        }
+
+        const newUser = new User();
+        newUser.email = createUserDto.email;
+        newUser.adress = createUserDto.adress;
+        newUser.state = createUserDto.state;
+        newUser.birthdate = createUserDto.birthdate;
+        newUser.phonenumber = createUserDto.phonenumber;
+        newUser.username = createUserDto.username;
+        newUser.lastname = createUserDto.lastname;
+        newUser.selfie = createUserDto.selfie;
+        newUser.dniFront = createUserDto.dniFront;
+        newUser.dniBack = createUserDto.dniBack;
+        newUser.documentDate = createUserDto.documentDate;
+        newUser.documentType = createUserDto.documentType;
+        newUser.documentNumber = createUserDto.documentNumber;
+        newUser.role = createUserDto.role;
+        newUser.recoverHash = createUserDto.recoverHash;
+        return this.userRepository.save(newUser);
+    }
 
     async findOne(email: string): Promise<User | undefined> {
         const userExists = await this.userRepository.findOne({
@@ -19,5 +52,55 @@ export class UserService {
             throw new UnauthorizedException('El usuario no existe.')
         }
         return userExists
+    }
+
+    async update(id: number, updateUserDto: UserUpdateDto) {
+        const userExists = await this.userRepository
+            .createQueryBuilder('role')
+            .where('role.id = :id', { id: id })
+            .getOne();
+        if (!userExists) {
+            throw new NotFoundException('No existe un rol con ese id');
+        }
+        const userExistsByName = await this.userRepository
+            .createQueryBuilder('role')
+            .where('role.name like :name', { name: `%${updateUserDto.username}%` })
+            .getOne();
+        if (userExistsByName && userExistsByName.id !== Number(id)) {
+            throw new ConflictException('Ya existe un rol con ese nombre');
+        }
+        userExists.email = updateUserDto.email ? updateUserDto.email : userExists.email;
+        userExists.adress = updateUserDto.adress ? updateUserDto.adress : userExists.adress;
+        userExists.state = updateUserDto.state ? updateUserDto.state : userExists.state;
+        userExists.birthdate = updateUserDto.birthdate ? updateUserDto.birthdate : userExists.birthdate;
+        userExists.phonenumber = updateUserDto.phonenumber ? updateUserDto.phonenumber : userExists.phonenumber;
+        userExists.username = updateUserDto.username ? updateUserDto.username : userExists.username;
+        userExists.lastname = updateUserDto.lastname ? updateUserDto.lastname : userExists.lastname;
+        userExists.selfie = updateUserDto.selfie ? updateUserDto.selfie : userExists.selfie;
+        userExists.dniFront = updateUserDto.dniFront ? updateUserDto.dniFront : userExists.dniFront;
+        userExists.dniBack = updateUserDto.dniBack ? updateUserDto.dniBack : userExists.dniBack;
+        userExists.documentDate = updateUserDto.documentDate ? updateUserDto.documentDate : userExists.documentDate;
+        userExists.documentType = updateUserDto.documentType ? updateUserDto.documentType : userExists.documentType;
+        userExists.documentNumber = updateUserDto.documentNumber ? updateUserDto.documentNumber : userExists.documentNumber;
+        userExists.role = updateUserDto.role ? updateUserDto.role : userExists.role;
+        userExists.recoverHash = updateUserDto.recoverHash ? updateUserDto.recoverHash : userExists.recoverHash;
+
+        return this.userRepository.save(userExists);
+    }
+
+    async remove(id: number) {
+        let data = await this.userRepository
+            .createQueryBuilder('user')
+            .where('user.id = :id', { id: id })
+            .getOne();
+        if (!data) {
+            throw new ConflictException('No existe un user con ese id.');
+        }
+        await this.userRepository.delete({ id });
+        data = await this.userRepository
+            .createQueryBuilder('user')
+            .where('user.id = :id', { id: id })
+            .getOne();
+        return data
     }
 }
